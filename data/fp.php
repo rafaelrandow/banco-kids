b334e44035698714d0636ee3c8e88329513eaae4
Rafael Randow Grossi <rafaelrandow@gmail.com>
2022-08-21 18:27:39 -0300

    Add files via upload

--- data/fp.php
+++ data/fp.php
@@ -1,14 +1,14 @@
 <?php
     $currentDirectory = getcwd();
     $uploadDirectory = "/data/";
-    $p1 = $_POST['pontosEllen'];
-    $p2 = $_POST['pontosRafa'];
-    $p3 = $_POST['objRafa'];
-    $p4 = $_POST['pontosBia'];
-    $p5 = $_POST['objBia'];
+    $p1 = $_GET['pontosEllen'];
+    $p2 = $_GET['pontosRafa'];
+    $p3 = $_GET['objRafa'];
+    $p4 = $_GET['pontosBia'];
+    $p5 = $_GET['objBia'];
     $arquivo = fopen('pontos.json','w+');
 
-    $conteudo = "[{\"nome\":\"Ellen\",\"pontos\":$p1},{\"nome\":\"Rafa\",\"pontos\":$p2},{\"nome\":\"objRafa\",\"pontos\":$p3},{\"nome\":\"Bia\",\"pontos\":$p4},{\"nome\":\"objBia\",\"pontos\":$p5}]";
+    $conteudo = "[{\"nome\":\"Ellen\",\"pontos\":$p1},{\"nome\":\"Rafa\",\"pontos\":$p2},{\"nome\":\"objRafa\",\"pontos\":\"$p3\"},{\"nome\":\"Bia\",\"pontos\":$p4},{\"nome\":\"objBia\",\"pontos\":\"$p5\"}]";
 
     if ($arquivo) {
       if (!fwrite($arquivo, $conteudo)) die('Não foi possível atualizar o arquivo.');
